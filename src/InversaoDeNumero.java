8230ad38f2753d9f54dccc97245cc6587851fb53
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-09 19:46:16 -0300

    feat: insere função de inverter numero

---
+++ src/InversaoDeNumero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InversaoDeNumero {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int number = sc.nextInt();
+
+        int numberConverted = 0;
+
+        while (number > 0) {
+            numberConverted = numberConverted *10;
+            numberConverted = numberConverted + (number % 10);
+            number = number / 10;
+        }
+        System.out.println(numberConverted);
+
+        sc.close();
+    }
+}
