7c5fda8e1522450cd66100dd582a68f4982d5db5
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-09 16:18:43 -0300

    feat: insere função soma de números

---
+++ src/SomaDeNumeros.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SomaDeNumeros{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int soma = 0;
+
+        System.out.printf("Digite um número: ");
+        int number = scanner.nextInt();
+
+        for (int i = 0; i <= number ; i++){
+            //System.out.println(i + "+");
+            soma += i;
+            System.out.println("A soma dos números 1 a " + number + " é igual a : " + soma);
+        }
+
+        scanner.close();
+    }
+}
+
+
+
+
