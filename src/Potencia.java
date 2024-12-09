af828aa19509cbda3b5e83c8e0ea9cc77da68d7c
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-09 20:27:21 -0300

    feat: insere função de potencia

---
+++ src/Potencia.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Potencia {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o número base: ");
+        int numberBase = input.nextInt();
+        System.out.println("Digite o número da potência: ");
+        int numberPotencia = input.nextInt();
+
+        System.out.println(expo(numberBase, numberPotencia));
+
+        input.close();
+    }
+
+    private static int expo(int x, int y) {
+        if (y == 0 || y == 1)
+            return x;
+
+        int total = x;
+
+        for (int i = 1; i < y; i++) {
+            total *= x;
+        }
+        return total;
+    }
+}
+
+
